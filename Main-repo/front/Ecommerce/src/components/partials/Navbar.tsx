c83b72a7c1d6c709d386f97549de46c2fe6963d3
mounahamdi <mounahamdi72@gmail.com>
2023-04-18 21:12:39 +0100

    registration front

---
+++ Main-repo/front/Ecommerce/src/components/partials/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Link} from 'react-router-dom'
+const Navbar = () => {
+    return (
+    <div>
+        <ul>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/register'>Register</Link></li>
+        </ul>
+    </div>
+    )
+}
+export default Navbar;
