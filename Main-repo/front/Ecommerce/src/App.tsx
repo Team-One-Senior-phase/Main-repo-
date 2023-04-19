8c9655e0246559cedf2341a64737d677fef0a083
mounahamdi <mounahamdi72@gmail.com>
2023-04-19 18:19:34 +0100

    19/04 commit

--- Main-repo/front/Ecommerce/src/App.tsx
+++ Main-repo/front/Ecommerce/src/App.tsx
@@ -1,16 +1,29 @@
-import React,{FC,useState} from 'react';
-import {Routes, Route} from 'react-router-dom'
+import React,{FC,useEffect,useState} from 'react';
+import {Routes, Route, useNavigate} from 'react-router-dom'
 import Register from './components/Register'
+import Login from './components/Login'
 import Navbar from './components/partials/Navbar'
 import Home from './Pages/Home'
 import axios from 'axios'
 
 const App:FC=() => {
   const [updated,setUpdated] = useState<boolean>(false)
+  const navigate = useNavigate()
 
   var registerUser = (name:string,mail:string,password:string): void =>{
     axios.post("http://localhost:3000/api/users/register",{user_name:name,email:mail,password:password})
-    .then(() => setUpdated(!updated))
+    .then(()=>setUpdated(!updated))
+  }
+
+  var loginUser = (mail:string,password:string):void =>{
+    axios.post("http://localhost:3000/api/users/login",{email:mail,password:password})
+    .then(response => {
+      console.log(response.data.message)
+      if (response.data.message === "User logged in successfully") {
+        navigate("/")
+      }
+    })
+    .catch(() => console.log("invalid user"))
   }
 
   return (
@@ -19,6 +32,7 @@ const App:FC=() => {
     <Routes>
       <Route path='/' element={<Home />}/>
       <Route path='/register' element={<Register registerUser={registerUser}/>}/>
+      <Route path='/login' element={<Login loginUser={loginUser}/>}/>
     </Routes>
     </>
   )
